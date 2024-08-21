a5fe4c47ec43e78ad90cebb76f071a89d09329d4
stork11 <126308978+stork11@users.noreply.github.com>
2024-08-21 20:44:17 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -73,6 +73,8 @@ iptables -P OUTPUT ACCEPT
 netfilter-persistent save //保存规则文件
 netfilter-persistent reload //载入规则文件
 
+crontab -l | { cat; echo "3 4 * * * systemctl restart docker"; } | crontab -
+
 docker run -it --rm -e ZONE=gotout.work -e SUBDOMAIN=jp1 xwhales/ddns:go
 
 bash /root/ws.sh
