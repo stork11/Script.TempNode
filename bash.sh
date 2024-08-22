66a68d6a12f2e3e22df4d06088fff0f33bb9503a
stork11 <126308978+stork11@users.noreply.github.com>
2024-08-22 15:27:25 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -64,3 +64,5 @@ docker run -d --restart=always --name qz1 -d -v /go/soga/:/etc/soga/ --network h
 docker run -d --restart=always --name lv1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest --type=v2board --server_type=trojan --api=webapi --webapi_url=https://soga.lvdengjiasu.top --webapi_key=abc1112223344dasdas --cert_domain=jp1.gotout.work --cert_mode=dns --cert_key_length=ec-256 --dns_provider=dns_cf --DNS_CF_Email=x.stork@protonmail.com --DNS_CF_Key=257c340dc19d72e2ee3999355e031c6c6831a --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --redis_enable=true --redis_addr=redis.wocao.one:6379 --redis_password=pluto.. --redis_db=9 --proxy_protocol=true --user_conn_limit=10 --user_tcp_limit=1000 --soga_key=7Hl0nBVPXWsZTiH7bqyBQQuoqAEqSFqo --node_id=31
 
 bash <(curl -sSL "https://raw.githubusercontent.com/stork11/Script.TempNode/main/after.sh")
+
+docker run -it --rm -e ZONE=gotout.work -e SUBDOMAIN=kr1 xwhales/ddns:go
