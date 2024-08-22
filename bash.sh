07f3fd14238544f8bccca0594680ef5663b788cb
stork11 <126308978+stork11@users.noreply.github.com>
2024-08-22 14:57:19 +0800

    a

--- bash.sh
+++ bash.sh
@@ -1,8 +1,12 @@
 bash <(curl -Ls neko.nnr.moe/iiii.sh)
 bash <(curl -sSL "https://gitlab.com/xx.stork/script/-/raw/master/auth.sh")
 
+mkdir /go
+mkdir /go/soga
+
+
 docker run -d --name=go1 --restart=always --log-opt max-size=5m --log-opt max-file=3 -v /go/soga/:/etc/soga/ -p 30001:30000 -p 30001:30000/udp vaxilu/soga:latest --type=sspanel-uim --api=webapi --webapi_url=https://soga.gogoo.work --webapi_key=gotout --proxy_protocol=true --soga_key=Zs3PGbfnYNZB7Eke4LASi0rXvdFWOV1C  --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --server_type=ss --node_id=151
-docker run -d --name=go-s2 --restart=always --log-opt max-size=5m --log-opt max-file=3 -v /go/soga/:/etc/soga/ -e server_type=ss -e node_id=14501 -p 32501:30000 -p 32501:30000/udp xwhales/soga:go --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --server_type=ss --node_id=14501 
+docker run -d --name=go-s2 --restart=always --log-opt max-size=5m --log-opt max-file=3 -v /go/soga/:/etc/soga/ -p 32501:30000 -p 32501:30000/udp vaxilu/soga:latest --type=sspanel-uim --api=webapi --webapi_url=https://soga.gogoo.work --webapi_key=gotout --proxy_protocol=true --soga_key=Zs3PGbfnYNZB7Eke4LASi0rXvdFWOV1C  --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --server_type=ss --node_id=14501
 docker run -d --name=go-ss3 --restart=always --log-opt max-size=5m --log-opt max-file=3 -p 30551:30000/tcp -p 30551:30000/udp  vaxilu/soga:latest --type=sspanel-uim --api=webapi --webapi_url=https://soga.gogoo.work --webapi_key=gotout --proxy_protocol=true --soga_key=Zs3PGbfnYNZB7Eke4LASi0rXvdFWOV1C  --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --server_type=ss --node_id=50051
 
 docker run -d --restart=always --name hd1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest  --type=v2board --server_type=v2ray --api=webapi --webapi_url=https://webapi.starcloud.site --webapi_key=8ca4ddbd00f2e2dd --proxy_protocol=true --soga_key=1lg9lhdQdixhup1NzFqCjOfEXzss1DPx --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --node_id=151
@@ -22,17 +26,9 @@ docker run -d --restart=always --name rck1 -d -v /go/soga/:/etc/soga/ --network 
 
 docker run -d --restart=always --name nc2 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest --type=v2board --server_type=trojan --api=webapi --webapi_url=https://soga.naichayun.net --webapi_key=zg1vAcLcI3OB46Qi --proxy_protocol=true --soga_key=Yn2GSGD1JLSXnwTQJp8wP27WF1MrPEfk --cert_domain=kr1.gotout.work --cert_mode=dns --cert_key_length=ec-256 --dns_provider=dns_cf --DNS_CF_Email=x.stork@protonmail.com --DNS_CF_Key=257c340dc19d72e2ee3999355e031c6c6831a  --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --redis_enable=true --redis_addr=redis.wocao.one:6379 --redis_password=pluto.. --redis_db=11 --user_conn_limit=10 --dy_limit_enable=true  --node_id=51
 
-docker run --restart=always --name azi1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest \
---type=v2board \
---server_type=v2ray \--soga_key=I9sEWoQSDDrZwOmPwqrPYvyJPlLS7Qa7 \
---api=webapi \--webapi_url=https://clientapi.azzico.cc/ \
---webapi_key=rRLt7ehPWgQXKxRWTJTf \--conn_limit_expiry=60 \
---proxy_protocol=true \--force_proxy_protocol=true \
---forbidden_ports=25,26,465,587,143,218,220,24,50,57,105,106,158,209,1109,24554,60177,60179 \
---node_id=151
+docker run -d --restart=always --name azi1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest --type=v2board --server_type=v2ray --soga_key=I9sEWoQSDDrZwOmPwqrPYvyJPlLS7Qa7 --api=webapi --webapi_url=https://clientapi.azzico.cc/ --webapi_key=rRLt7ehPWgQXKxRWTJTf --conn_limit_expiry=60 --proxy_protocol=true --force_proxy_protocol=true --forbidden_ports=25,26,465,587,143,218,220,24,50,57,105,106,158,209,1109,24554,60177,60179 --node_id=151
 
 docker run -d --restart=always --name xz1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest --type=v2board --server_type=ss --api=webapi --webapi_url=https://api.xianzheyun.top --webapi_key="m0&]MH&sgqp3GSuF?T(a" --proxy_protocol=true --soga_key=6xT4xSMciKT2DthE1BHpuoBqKJQgJd0t --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --node_id=151,152
-
 
 docker run -d --restart=always --name km1 -d -v /go/soga/:/etc/soga/ --network host vaxilu/soga:latest --type=v2board --server_type=ss --api=webapi --webapi_url=https://pannel.kuaimao.in --webapi_key=Kuaimao20240125K --forbidden_ports=22,24,25,26,50,57,105,106,109,110,143,158,209,218,220,465,587,993,995,1109 --proxy_protocol=true --redis_enable=true --redis_addr=redis.wocao.one:6379 --redis_password=pluto.. --redis_db=13 --user_conn_limit=5 --user_tcp_limit=1000 --soga_key=ZKNBvIyMaA97KGunhcTfeVjW0ggothWJ --node_id=151
 
