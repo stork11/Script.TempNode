aef945d2a8897858c632d677b77fe07aacd6e1be
stork11 <126308978+stork11@users.noreply.github.com>
2024-08-22 15:06:53 +0800

    aaa

---
+++ before.sh
@@ -0,0 +1,7 @@
+bash <(curl -Ls neko.nnr.moe/iiii.sh)
+bash <(curl -sSL "https://gitlab.com/xx.stork/script/-/raw/master/auth.sh")
+
+mkdir /go
+mkdir /go/soga
+wget -N --no-check-certificate "https://raw.githubusercontent.com/stork11/Script.TempNode/main/blockList"
+mv /root/blockList /go/soga/blockList
